25298ef5af15a989feadf6db7e3dd35b89a2b286
Manuel Ivan Alzamora Flores <manolo_1505@hotmail.com>
2019-06-01 16:13:25 -0500

    services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
-  constructor() { }
+
+export class UserService {
+  private extractData(res: Response) {
+    const body = res;
+    return body || { };
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+  constructor(private http: HttpClient) { }
 }
