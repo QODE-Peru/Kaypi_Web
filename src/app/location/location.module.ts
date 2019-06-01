0f013cee63c10a4f1a70774bcc2e658d3a3da398
Manuel Ivan Alzamora Flores <manolo_1505@hotmail.com>
2019-06-01 14:07:05 -0500

    login

--- src/app/location/location.module.ts
+++ src/app/location/location.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchComponent, SearchBoxComponent],
   imports: [
     CommonModule
   ]
